2746b1f577f796d815d4b2b5cd65a2b2192ba70f
Jonah Burstein <jburstei@u.rochester.edu>
2018-01-23 17:15:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,9 @@ var playlist = new Object({ artistName : 'songTitle'});
 
 function updatePlaylist (playlist, artistName, songTitle){
   //playlist.firstSong = {Phil Ochs : 'Here\'s to the State of Mississippi'}
-  playlist.philOchs = 'Here\'s to the State of Mississippi';
-}updatePlaylist()
+  // playlist.philOchs = 'Here\'s to the State of Mississippi';
+  playlist[artistName] = songTitle
+  return playlist
+}updatePlaylist(playlist, 'Phil Ochs', 'Here\'s to the State of Mississippi')
 
 //'Phil Ochs', "Here's to the State of Mississippi")).
