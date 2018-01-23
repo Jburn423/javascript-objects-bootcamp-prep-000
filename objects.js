764e72c59f5668661efcd4a84b335ec94d9455d7
Jonah Burstein <jburstei@u.rochester.edu>
2018-01-23 17:16:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,5 +4,3 @@ function updatePlaylist (playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   return playlist
 }updatePlaylist(playlist, 'Phil Ochs', 'Here\'s to the State of Mississippi')
-
-//'Phil Ochs', "Here's to the State of Mississippi")).
