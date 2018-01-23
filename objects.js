dcfe8787fa758426d3809f996244040ab0c3ef60
Jonah Burstein <jburstei@u.rochester.edu>
2018-01-23 17:19:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,4 +8,4 @@ function updatePlaylist (playlist, artistName, songTitle){
 function removeFromPlaylist(playlist, artistName, songTitle){
   // delete playlist[artistName] = songTitle;
   delete artistName.songTitle;
-}
+}removeFromPlaylist(playlist, 'Phil Ochs', 'Here\'s to the State of Mississippi');
