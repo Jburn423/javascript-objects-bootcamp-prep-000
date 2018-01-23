d4df2e963cb2674b41836aba21346b16287a8dec
Jonah Burstein <jburstei@u.rochester.edu>
2018-01-23 17:22:20 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,4 +8,4 @@ function updatePlaylist (playlist, artistName, songTitle){
 function removeFromPlaylist(playlist, artistName, songTitle){
   delete playlist.songTitle;
   return playlist;
-}removeFromPlaylist(playlist, 'Kanye', 'Here\'s to the State of Mississippi');
+}removeFromPlaylist(playlist, 'Kanye', 'Gold Digger');
